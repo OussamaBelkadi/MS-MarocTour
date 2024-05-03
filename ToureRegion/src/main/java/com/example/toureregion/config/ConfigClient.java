503affbe6882e5b9d7116b913c7381c36340f2ea
OussamaBelkadi <OussamaYC@hotmail.com>
2024-05-03 10:04:58 +0000

    add first version for the global service for the region service

--- ToureRegion/src/main/java/com/example/toureregion/config/ConfigClient.java
+++ ToureRegion/src/main/java/com/example/toureregion/config/ConfigClient.java
@@ -1,7 +1,18 @@
 package com.example.toureregion.config;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
-@ConfigurationProperties("global.params")
-public record ConfigClient(int a, int b) {
+@ConfigurationProperties(prefix = "global.params")
+@RefreshScope()
+@Getter @NoArgsConstructor @AllArgsConstructor
+public class ConfigClient {
+    private int a;
+    private int b;
+
 }
